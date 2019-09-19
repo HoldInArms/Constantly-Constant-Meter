5fb9b4e010911e842a844bd83904979c3394adcc
ronaim <ronaimate95@gmail.com>
2019-09-19 16:24:06 +0200

    Add initial application structure.

---
+++ src/main/kotlin/hu/holdinarms/constantlyconstantmeter/ConstantlyConstantMeterApplication.kt
@@ -0,0 +1,11 @@
+package hu.holdinarms.constantlyconstantmeter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConstantlyConstantMeterApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConstantlyConstantMeterApplication>(*args)
+}
